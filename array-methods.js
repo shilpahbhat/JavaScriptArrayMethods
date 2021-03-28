9aff67556f623712f47b4cd6cbe4846e4de94607
Shilpa Bhat <64108941+shilpahbhat@users.noreply.github.com>
2021-03-28 11:58:06 -0700

    reduce, some

--- array-methods.js
+++ array-methods.js
@@ -52,7 +52,7 @@ console.log(Object.keys(map).filter(key => map[key] === 'Ron'))
 // every
 
 const numbers = [3, -5, 6, 6, -6, 0]
-console.log(numbers.every(number=> number >0))
+console.log(numbers.every(number=> number > 0))
 
 function mutation(arr) {
   return arr[1].toLowerCase().split("").
@@ -64,4 +64,46 @@ console.log(mutation(["hello", "lehlo"]));
 // some
 
 const numbers = [3, -5, -6, 6, -6, 0]
-console.log(numbers.some(number=> number >0))
+console.log(numbers.some(number=> number > 0))
+
+let indexOfUser = -1;
+const userFound = users.some((user, index)=>{
+  let isFound = user.name === 'John';
+  if(isFound){
+    indexOfUser = index;
+  }
+  return isFound;
+})
+console.log(indexOfUser, userFound)
+
+// reduce
+
+const sum = numbers.reduce(function(accumulator, number) {
+  return accumulator + number; 
+});
+console.log(sum); 
+
+const doublesSum = numbers.reduce(function (accumulator, number) {
+  return accumulator + number * 2;
+}, 10);
+console.log(doublesSum);
+
+const coordinates = [
+  { x: 1, y: 2 }, 
+  { x: 2, y: 3 }, 
+  { x: 3, y: 4 }
+];
+const sum = coordinates.reduce(function (accumulator, currentValue) {
+    return accumulator + currentValue.x;
+}, 0);
+console.log(sum); 
+
+const sumOfAges = users.reduce((sum, user) => sum + user.age, 0);
+console.log(sumOfAges);
+
+const usersObj = users.reduce((obj, user) => {
+  obj[user.name] = user.age;
+  return obj;
+}, {});
+console.log(usersObj); 
+
